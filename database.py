75c3d4c13e1dcd3b766f57424031824ad9a6c372
Lowstz Chen <lowstz@gmail.com>
2013-02-11 02:44:39 +0800

    make exception information in friendly

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 
 import config
+import sys
 
 import sqlalchemy as sa
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,6 +19,11 @@ mBase = declarative_base()
 
 def create_db():
     import models
-    mBase.metadata.create_all(engine)
+    try:
+        mBase.metadata.create_all(engine)
+    except OperationalError, err:
+        sys.stderr.write('OperationalError: %s\n' %  str(err))
+        print("目测是数据库没有权限或不存在，自己不会看OpertaionalError啊混蛋！")
+        sys.exit(1)
     print("数据库部署完成！")
     return
